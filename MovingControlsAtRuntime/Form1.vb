12b951d3671a92d481849cb4812403fd2d0fd42b
Edward G Trenchard <ed_trench@yahoo.com>
2012-10-15 16:55:24 +0100

    15-10-2012

---
+++ MovingControlsAtRuntime/Form1.vb
@@ -0,0 +1,39 @@
+﻿Public Class Form1
+
+    Private dragging As Boolean
+    Private beginX As Integer
+    Private beginY As Integer
+
+    Private Sub Control_MouseDown(sender As System.Object, e As System.Windows.Forms.MouseEventArgs) ' Handles PictureBox1.MouseDown
+
+        Me.dragging = True
+        Me.beginX = e.X
+        Me.beginY = e.Y
+
+    End Sub
+
+    Private Sub Control_MouseMove(sender As System.Object, e As System.Windows.Forms.MouseEventArgs) 'Handles PictureBox1.MouseMove
+
+        Dim cntrl As Control = CType(sender, Control)
+
+        If Me.dragging Then
+            cntrl.Location = New Point(cntrl.Location.X + e.X - beginX, cntrl.Location.Y + e.Y - beginY)
+            Me.Refresh()
+        End If
+
+    End Sub
+
+    Private Sub Control_MouseUp(sender As Object, e As System.Windows.Forms.MouseEventArgs) 'Handles PictureBox1.MouseUp
+        Me.dragging = False
+    End Sub
+
+    Private Sub Form1_Load(sender As System.Object, e As System.EventArgs) Handles MyBase.Load
+
+        For Each cntrl As Control In Me.Controls
+            AddHandler cntrl.MouseDown, AddressOf Control_MouseDown
+            AddHandler cntrl.MouseMove, AddressOf Control_MouseMove
+            AddHandler cntrl.MouseUp, AddressOf Control_MouseUp
+        Next
+
+    End Sub
+End Class
---
+++ MovingControlsAtRuntime/My Project/AssemblyInfo.vb
@@ -0,0 +1,35 @@
+﻿Imports System
+Imports System.Reflection
+Imports System.Runtime.InteropServices
+
+' General Information about an assembly is controlled through the following 
+' set of attributes. Change these attribute values to modify the information
+' associated with an assembly.
+
+' Review the values of the assembly attributes
+
+<Assembly: AssemblyTitle("MovingControlsAtRuntime")> 
+<Assembly: AssemblyDescription("")> 
+<Assembly: AssemblyCompany("Asdan")> 
+<Assembly: AssemblyProduct("MovingControlsAtRuntime")> 
+<Assembly: AssemblyCopyright("Copyright © Asdan 2012")> 
+<Assembly: AssemblyTrademark("")> 
+
+<Assembly: ComVisible(False)>
+
+'The following GUID is for the ID of the typelib if this project is exposed to COM
+<Assembly: Guid("54d95d1d-5793-4e3e-bb61-59622347344f")> 
+
+' Version information for an assembly consists of the following four values:
+'
+'      Major Version
+'      Minor Version 
+'      Build Number
+'      Revision
+'
+' You can specify all the values or you can default the Build and Revision Numbers 
+' by using the '*' as shown below:
+' <Assembly: AssemblyVersion("1.0.*")> 
+
+<Assembly: AssemblyVersion("1.0.0.0")> 
+<Assembly: AssemblyFileVersion("1.0.0.0")> 
